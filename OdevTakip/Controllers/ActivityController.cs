4641c9b5201029b54ef2b0cc6afadd72de699a5a
Necmettin Cimen <necmettincimen01@gmail.com>
2019-04-09 17:12:18 +0300

    etkinlik controller eklendi

---
+++ OdevTakip/Controllers/ActivityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OdevTakip.Services;
+
+namespace OdevTakip.Controllers
+{
+    public class ActivityController : Controller
+    {
+        private readonly IEtkinlikService _etkinlikService;
+
+        public ActivityController(IEtkinlikService etkinlikService)
+        {
+            _etkinlikService = etkinlikService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
