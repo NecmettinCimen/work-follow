e4ef8ebc65f94e65d6137e53a8462ce780717522
Necmettin Çimen <necmettin@tuga.com.tr>
2019-04-09 15:43:40 +0300

    etkinlik eklendi

---
+++ OdevTakip.Entities/Etkinlik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OdevTakip.Entities
+{
+    public class Etkinlik : BaseEntity
+    {
+        public string baslik { get; set; }
+        public string aciklama { get; set; }
+        public int durumid { get; set; }
+        public int kategoriid { get; set; }
+        public DateTime baslangictarihi { get; set; }
+        public DateTime bitistarihi { get; set; }
+        public int atananid { get; set; }
+        public int projeid { get; set; }
+    }
+}
---
+++ OdevTakip.Services/EtkinlikService.cs
@@ -0,0 +1,74 @@
+﻿using OdevTakip.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdevTakip.Services
+{
+    public interface IEtkinlikService
+    {
+        List<Etkinlik> Select(Etkinlik model);
+
+        Etkinlik Find(Etkinlik model);
+
+        bool Insert(Etkinlik model);
+
+        bool Update(Etkinlik model);
+
+        bool Delete(Etkinlik model);
+
+    }
+
+    public class EtkinlikService : IEtkinlikService
+    {
+        private readonly IGenericRepository _genericRepository;
+        public EtkinlikService(IGenericRepository genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+        public List<Etkinlik> Select(Etkinlik model)
+        {
+            const string sql = @"SELECT id, sil, olusturmatarihi, olusturankisi, guncellemetarihi, guncelleyenkisi, baslik, aciklama, durumid, kategoriid, baslangictarihi, bitistarihi, atananid, projeid
+	                                FROM public.etkinlik where sil = @sil and olusturankisi=@olusturankisi;";
+
+            return _genericRepository.Select<Etkinlik>(sql, model);
+        }
+
+        public Etkinlik Find(Etkinlik model)
+        {
+            const string sql = @"SELECT id, sil, olusturmatarihi, olusturankisi, guncellemetarihi, guncelleyenkisi, baslik, aciklama, durumid, kategoriid, baslangictarihi, bitistarihi, atananid, projeid
+	                                FROM public.etkinlik where sil = @sil and olusturankisi=@olusturankisi and id=@id;";
+
+            return _genericRepository.First<Etkinlik>(sql, model);
+        }
+
+        public bool Insert(Etkinlik model)
+        {
+            const string sql = @"INSERT INTO public.etkinlik(
+	                                sil, olusturmatarihi, olusturankisi, guncellemetarihi, guncelleyenkisi, baslik, aciklama, durumid, kategoriid, baslangictarihi, bitistarihi, atananid, projeid)
+	                                VALUES (@sil, @olusturmatarihi, @olusturankisi, @guncellemetarihi, @guncelleyenkisi, @baslik, @aciklama, @durumid, @kategoriid, @baslangictarihi, @bitistarihi, @atananid, @projeid);";
+
+            return _genericRepository.Update(sql, model);
+        }
+
+        public bool Update(Etkinlik model)
+        {
+            const string sql = @"UPDATE public.etkinlik
+	                                SET guncellemetarihi=@guncellemetarihi, guncelleyenkisi=@guncelleyenkisi, baslik=@baslik, aciklama=@aciklama, durumid=@durumid, kategoriid=@kategoriid, baslangictarihi=@baslangictarihi, bitistarihi=@bitistarihi, atananid=@atananid, projeid=@projeid
+	                                WHERE id=@id";
+
+            return _genericRepository.Update(sql, model);
+        }
+
+        public bool Delete(Etkinlik model)
+        {
+            const string sql = @"UPDATE public.etkinlik
+	                                SET sil=true
+	                                WHERE id=@id";
+
+            return _genericRepository.Delete(sql, model);
+        }
+
+    }
+}
